55545741deef418d3b45425b5f87b4bcab67ad0b
brent <brent.wish@gmail.com>
2017-08-30 15:26:44 -0700

    Sorts InfoPane content into bootstrap tabs.

--- hive/src/private/react/InfoPane.js
+++ hive/src/private/react/InfoPane.js
@@ -2,8 +2,22 @@ import React, { Component } from 'react';
 import TileInfo from "./TileInfo.js";
 import PlayerInfo from "./PlayerInfo.js";
 
+import { Tab, Tabs } from "react-bootstrap";
+
 class InfoPane extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      currentTab: props.activeTab || 1
+    };
+  }
+  handleSelect = (tab) => {
+    this.setState({ currentTab: tab});
+  }
   render() {
+    const playerInfo = (
+      <PlayerInfo players={ this.props.players }/>
+    );
     let tileInfo;
     if (window.hive && this.props.watchTile) {
       tileInfo = (
@@ -13,11 +27,19 @@ class InfoPane extends Component {
         />
       );
     }
-    const playerInfo = <PlayerInfo players={ this.props.players }/>;
     return (
       <div className="InfoPanel">
-        { playerInfo }
-        { tileInfo }
+        <Tabs
+          activeKey={ this.state.currentTab }
+          onSelect={ this.handleSelect }
+        >
+          <Tab eventKey={ 1 } title="Player Info">
+            { playerInfo }
+          </Tab>
+          <Tab eventKey={ 2 } title="Tile Info">
+            { tileInfo }
+          </Tab>
+        </Tabs>
       </div>
     );
   }
