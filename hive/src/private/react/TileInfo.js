e591bcbe5dceac8329c7643823cf98860e1775c7
DownSpiral <eric.d.wishart@gmail.com>
2017-08-31 21:41:01 -0700

    Refactor TileInfo to use tables
    Also styles dem tables.

--- hive/src/private/react/TileInfo.js
+++ hive/src/private/react/TileInfo.js
@@ -5,55 +5,64 @@ import { capitalize } from "../js/constants.js"
 
 class TileInfo extends Component {
   render() {
-    let tileDiv = [
-      <h3>Tile</h3>
-    ];
-    let antDiv = [
-      <h3>Ant</h3>
-    ];
-
+    let tileTable;
+    let antTable;
     if (this.props.x && this.props.y) {
       const tile = window.hive.board.getTileFromCoords({
         x: this.props.x,
         y: this.props.y
       });
-      const tileHash = tile.toDataHash();
-      const trailElements = (
-        <ul>{
-          _.map(tileHash.trails, (strength, name) => {
-            return (<li>{ name }: { strength }</li>);
-          })
-        }</ul>
-      );
-      tileDiv.push(<span>Tile type: { capitalize(tileHash.type) }</span>);
-      tileDiv.push(<span>Food: { tileHash.food || "0" }</span>);
-      tileDiv.push(<span>Trails: { trailElements }</span>);
 
-      const ant = tile.ant ? tile.ant.toDataHash() : null;
-      if (ant) {
-        antDiv.push(<span>Owner: { ant.ownerId }</span>);
-        antDiv.push(<span>Ant type: { capitalize(ant.type) }</span>);
-        antDiv.push(<span>Food: { ant.carryingAmount }</span>);
-        antDiv.push(<span>Moves:
-          <ul>{
-            _.map(ant.moves, (n, dir) => {
-              return (<li>{ dir }: { n }</li>);
-            })
-          }</ul>
-        </span>);
-        antDiv.push(<span>Health: { ant.health }</span>);
-        antDiv.push(<span>Age: { ant.age }</span>);
-      } else {
-        antDiv.push(<h5>None</h5>);
+      if (tile) {
+        const trailTable = (
+          <table className="innerTable">
+            { _.map(tile.trails || [], (v, k) => { return <tr key={ k }><td>{ k }</td><td>{ v }</td></tr>; }) }
+          </table>
+        );
+        const tileInfo = {
+          "Type": capitalize(tile.type),
+          "Food": tile.food || "0",
+          "Trails": (_.isEmpty(tile.trails) ? "None" : trailTable),
+        };
+        tileTable = (
+          <table className="outerTable">
+            { _.map(tileInfo, (v, k) => { return <tr key={ k }><td>{ k }</td><td>{ v }</td></tr>; }) }
+          </table>
+        );
       }
-    } else {
-      tileDiv.push(<h5>None</h5>);
-      antDiv.push(<h5>None</h5>);
+
+      if (tile.ant) {
+        const ant = tile.ant;
+        const movesTable = (
+          <table className="innerTable">
+            { _.map(ant.moves, (v, k) => { return <tr key={ k }><td>{ k }</td><td>{ v }</td></tr>; }) }
+          </table>
+        );
+        const antInfo = {
+          "Owner": ant.owner.id,
+          "Type": capitalize(ant.type),
+          "Food": ant.food,
+          "Health": ant.health,
+          "Age": ant.age,
+          "Moves": movesTable,
+        };
+        antTable = (
+          <table className="outerTable">
+            { _.map(antInfo, (v, k) => { return <tr key={ k }><td>{ k }</td><td>{ v }</td></tr>; }) }
+          </table>
+        );
+      }
     }
     return (
-      <div className="TileInfoContainer">
-        <div>{ tileDiv }</div>
-        <div>{ antDiv }</div>
+      <div className="TileInfo">
+        <div className="Panel">
+          <div className="Title">Tile</div>
+          { tileTable }
+        </div>
+        <div className="Panel">
+          <div className="Title">Ant</div>
+          { antTable }
+        </div>
       </div>
     );
   }
