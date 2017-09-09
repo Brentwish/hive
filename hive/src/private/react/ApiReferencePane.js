06449bceecf0c8025ad98add9ec84a10e90b8e51
brent <brent.wish@gmail.com>
2017-09-09 14:53:43 -0700

    Adds ApiReference pane
    This commit adds react-markdown to package.json
    Adds a button on the editor pane that toggles the right pane to be a
    reference to the api.

--- hive/src/private/react/ApiReferencePane.js
+++ hive/src/private/react/ApiReferencePane.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import Markdown from "react-markdown";
+import "./ApiReferencePane.css"
 
 class ApiReferencePane extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      source: [
+        '# Hive',
+        'This is hive.',
+        '\n',
+        '### Code Block',
+        '```js',
+        'if (antData.type === "worker") {',
+        '  return {',
+        '    type: "move",',
+        '    direction: _.sample(dirs),',
+        '  }',
+        '}',
+        '```'
+      ].join("\n")
+    }
+  }
   render() {
     return (
       <div>
+        <Markdown
+          className="Markdown"
+          source={ this.state.source }
+        />
       </div>
     );
   }
