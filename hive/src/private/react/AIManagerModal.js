8246fded02e90894e88886a0b1717eec9ae853e3
DownSpiral <eric.d.wishart@gmail.com>
2017-09-09 12:36:04 -0700

    Add styling to AIManagerModal

--- hive/src/private/react/AIManagerModal.js
+++ hive/src/private/react/AIManagerModal.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import "./AIManagerModal.css";
 import { Modal, Button, Table } from 'react-bootstrap';
 import _ from "lodash";
 
@@ -15,14 +16,14 @@ class AIManagerModal extends Component {
   generateAITableRow = (ai) => {
     return (
       <tr>
-        <td>
-          <Button onClick={ () => this.props.selectAI(ai.id) }>
+        <td className="centered">
+          <Button bsStyle="primary" onClick={ () => this.props.selectAI(ai.id) }>
             <span className="glyphicon glyphicon-folder-open"/>
           </Button>
         </td>
-        <td><input value={ ai.name } onChange={ (e) => this.props.updateAI(ai.id, 'name', e.target.value) }/></td>
-        <td>
-          <Button onClick={ () => this.props.deleteAI(ai.id) }>
+        <td className="name"><input value={ ai.name } onChange={ (e) => this.props.updateAI(ai.id, 'name', e.target.value) }/></td>
+        <td className="centered">
+          <Button bsStyle="danger" onClick={ () => this.props.deleteAI(ai.id) }>
             <span className="glyphicon glyphicon-trash"/>
           </Button>
         </td>
@@ -33,24 +34,28 @@ class AIManagerModal extends Component {
     return (
       <Table>
         <thead>
-          <th></th>
-          <th>AI Name</th>
-          <th></th>
+          <th className="centered">Edit</th>
+          <th className="name">AI Name</th>
+          <th className="centered">Delete</th>
         </thead>
         <tbody>
           { _.map(this.props.AIs, this.generateAITableRow) }
+          <tr><td>
+            <Button bsStyle="success" onClick={ this.props.addAI }>
+              <span className="glyphicon glyphicon-plus"/>
+            </Button>
+          </td></tr>
         </tbody>
       </Table>
     );
   }
   render() {
     return (
-      <Modal show={ this.props.showModal } onHide={ this.props.close }>
+      <Modal className="AIManagerModal" show={ this.props.showModal } onHide={ this.props.close }>
         <Modal.Header closeButton>
           <Modal.Title>AI Manager</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Button onClick={ this.props.addAI }>Add AI</Button>
           { this.generateAITable() }
         </Modal.Body>
         <Modal.Footer>
