4417fc31fa7cc2ac315b411785ba106b2382e678
brent <brent.wish@gmail.com>
2017-09-28 11:45:41 -0700

    Factors displaying errors into its own component
    Also makes minor changes to Api

--- hive/src/Api.js
+++ hive/src/Api.js
@@ -1,11 +1,11 @@
 export const Api = [
   '# Hive',
-  '>Welcome to hive, a platform for designing AIs for a simple RTS style game.',
+  '### Welcome to hive, a platform for designing AIs for a simple RTS style game.',
   '>\n',
   '>There are two types of ants: queens and workers.  ',
   '>Queens are capable of laying eggs to hatch workers or more queens.  ',
   '>Workers, being much cheaper than queens, are expendable hunters, gatherers, explorers etc.  ',
-  '>Each Ant has the ability to lay a trail which other ants can detect and make decisions off of.',
+  '>Each Ant has the ability to lay a trail which other ants can detect and make decisions off of.  ',
   '```js',
   'if (antData.type === "worker") {',
   '  return {',
