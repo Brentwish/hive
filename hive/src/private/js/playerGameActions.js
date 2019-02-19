6ccaf7ff219ab9c29c6cf18887e8fc15d792362d
DownSpiral <eric.d.wishart@gmail.com>
2019-02-18 20:47:49 -0800

    Fix example AI
    This broken with the update to trails. Also fixes
    missing semicolons.

--- hive/src/private/js/playerGameActions.js
+++ hive/src/private/js/playerGameActions.js
@@ -1,4 +1,5 @@
 const { findKey, MAX_FOOD, NEW_ANT_COST, NEW_QUEEN_COST, STARTING_TRAIL_TIMER, dirs }  = constants;
+const FOOD_TRAIL = 0;
 
 const getDirsTowardsQueen = function(adjacentTilesHash, moves) {
   let toGo = [];
@@ -13,16 +14,16 @@ const getDirsTowardsQueen = function(adjacentTilesHash, moves) {
     toGo.push("up");
   }
   return toGo;
-}
+};
 
 const getDirsAwayFromQueen = function(adjacentTilesHash, moves) {
   return _.difference(dirs, getDirsTowardsQueen(adjacentTilesHash, moves));
-}
+};
 
 const distFromQueen = function(antData) {
-	const m = antData.moves;
-	return Math.abs(m.up - m.down) + Math.abs(m.left - m.right);
-}
+  const m = antData.moves;
+  return Math.abs(m.up - m.down) + Math.abs(m.left - m.right);
+};
 
 const getRandomDirAwayFromQueen = function(antData, makeRandomMove=true) {
   const adjacentTilesHash = antData.adjacentTiles;
@@ -30,7 +31,7 @@ const getRandomDirAwayFromQueen = function(antData, makeRandomMove=true) {
   const toGo = getDirsAwayFromQueen(adjacentTilesHash, moves);
   let closestEmptyTiles = toGo.filter((dir) => { return !adjacentTilesHash[dir].ant && adjacentTilesHash[dir].type !== "wall"; });
   if (closestEmptyTiles.length === 0 && makeRandomMove) {
-		closestEmptyTiles = _.difference(dirs, toGo);
+    closestEmptyTiles = _.difference(dirs, toGo);
   }
   return _.sample(closestEmptyTiles);
 }
@@ -41,44 +42,44 @@ const getRandomDirTowardsQueen = function(antData, makeRandomMove=true) {
   const toGo = getDirsTowardsQueen(adjacentTilesHash, moves);
   let closestEmptyTiles = toGo.filter((dir) => { return !adjacentTilesHash[dir].ant && adjacentTilesHash[dir].type !== "wall"; });
   if (closestEmptyTiles.length === 0 && makeRandomMove) {
-		closestEmptyTiles = _.difference(dirs, toGo);
+    closestEmptyTiles = _.difference(dirs, toGo);
   }
   return _.sample(closestEmptyTiles);
-}
+};
 
 const getOpenTiles = function(tiles) {
-	return _.pickBy(tiles, (tile) => {
-		return !tile.ant && tile.type !== "wall";
-	});
-}
+  return _.pickBy(tiles, (tile) => {
+    return !tile.ant && tile.type !== "wall";
+  });
+};
 
 const getTilesWithTrails = function(tiles, name) {
-	return _.pickBy(tiles, (tile) => {
-		return !_.isEmpty(tile.trails) && tile.trails[name];
-	});
-}
+  return _.pickBy(tiles, (tile) => {
+    return !_.isEmpty(tile.trails) && tile.trails[name];
+  });
+};
 
 const getForagingDir = function(antData) {
-	const openTiles = getOpenTiles(antData.adjacentTiles);
-	const tilesWithTrails = getTilesWithTrails(openTiles, `${antData.ownerId}_food`);
-	const dirsAwayFromQueen = getDirsAwayFromQueen(antData.adjacentTiles, antData.moves);
-	let trailDirs = _.intersection(_.keys(tilesWithTrails), dirsAwayFromQueen);
+  const openTiles = getOpenTiles(antData.adjacentTiles);
+  const tilesWithTrails = getTilesWithTrails(openTiles, `${antData.ownerId}_${FOOD_TRAIL}`);
+  const dirsAwayFromQueen = getDirsAwayFromQueen(antData.adjacentTiles, antData.moves);
+  let trailDirs = _.intersection(_.keys(tilesWithTrails), dirsAwayFromQueen);
   const spiralDir = spiral(antData, Math.PI / 200);
   const moveCount = _.sum(_.values(antData.moves));
-	if (!_.isEmpty(trailDirs)) {
-	  return _.sample(trailDirs);
-	} else if (_.includes(_.keys(openTiles), spiralDir) && moveCount < 2000) {
+  if (!_.isEmpty(trailDirs)) {
+    return _.sample(trailDirs);
+  } else if (_.includes(_.keys(openTiles), spiralDir) && moveCount < 2000) {
     return spiralDir;
-	} else {
+  } else {
     return _.sample(_.keys(openTiles));
   }
-}
+};
 
 const perpendicularDirections = function(dir) {
   const rl = ["right", "left"];
   const ud = ["up", "down"];
   return _.includes(rl, dir) ? ud : rl;
-}
+};
 
 const dirFromAngle = function(angle) {
   const r = Math.random();
@@ -95,7 +96,7 @@ const dirFromAngle = function(angle) {
       return ["up", "right", "left", "down"];
     }
   }
-}
+};
 
 const dirAwayFromQueen = function(antData, delta=Math.PI) {
   const moves = antData.moves;
@@ -106,21 +107,21 @@ const dirAwayFromQueen = function(antData, delta=Math.PI) {
 
   const angle = atan - delta;
   const orderedDirs = dirFromAngle(angle);
-	const openDirs = _.keys(getOpenTiles(antData.adjacentTiles));
+  const openDirs = _.keys(getOpenTiles(antData.adjacentTiles));
   if (!_.isEmpty(openDirs)) {
     return _.intersection(orderedDirs, openDirs)[0];
   } else {
     return "none";
   }
-}
+};
 
 const dirTowardsQueen = function(antData) {
   return dirAwayFromQueen(antData, 0);
-}
+};
 
 const spiral = function(antData, delta) {
   return dirAwayFromQueen(antData, (Math.PI / 2) - delta);
-}
+};
 
 const returnToQueenAction = function(antData) {
   const queenTile = _.values(antData.adjacentTiles).find((t) => {
@@ -141,21 +142,21 @@ const returnToQueenAction = function(antData) {
       action.direction = _.sample(_.keys(getOpenTiles(antData.adjacentTiles)));
     } else {
       const openTiles = getOpenTiles(antData.adjacentTiles);
-      const trailTiles = getTilesWithTrails(openTiles, `${antData.ownerId}_food`);
+      const trailTiles = getTilesWithTrails(openTiles, `${antData.ownerId}_${FOOD_TRAIL}`);
       const trailDirs = _.sortBy(_.keys(trailTiles), (tDir) => -1 * trailTiles[tDir].trails[`${antData.ownerId}_food`]);
       const toQueen = dirTowardsQueen(antData);
       const trailsToQueen = _.intersection(trailDirs, _.concat(perpendicularDirections(toQueen), toQueen));
       action.direction = _.isEmpty(trailsToQueen) ? toQueen : _.sample([toQueen, trailsToQueen[0]]);
       if (distFromQueen(antData) > 4) {
         action.trail = {
-          name: "food",
+          channel: FOOD_TRAIL,
           strength: 150,
         };
       }
     }
     return action;
   }
-}
+};
 
 return (antData) => {
   const adjacentTiles = Object.values(antData.adjacentTiles);
@@ -167,7 +168,7 @@ return (antData) => {
       return {
         type: "attack",
         direction: findKey(antData.adjacentTiles, _.sample(adjacentEnemies)),
-      }
+      };
     } else if (antData.age < 100) {
       return {
         type: "move",
@@ -205,12 +206,12 @@ return (antData) => {
       return {
         type: "attack",
         direction: findKey(antData.adjacentTiles, _.sample(adjacentEnemies)),
-      }
+      };
     } else if (adjacentFoodTiles.length > 0) {
       return {
         type: "gather",
         direction: findKey(antData.adjacentTiles, _.sample(adjacentFoodTiles)),
-      }
+      };
     } else {
       return {
         type: "move",
@@ -218,4 +219,4 @@ return (antData) => {
       };
     }
   }
-}
+};
